5c91eb03b7ee0b864db1fbcf1ffb32238c2e6ed3
Renato Groffe <renato.groffe@yahoo.com.br>
2020-08-01 15:45:51 -0300

    Docker + Docker Compose + SQL Server + MongoDB + Apache Kafka
    Docker + Docker Compose + SQL Server + MongoDB + Apache Kafka

---
+++ SQLServer/sqlserver2017.sh
@@ -0,0 +1,1 @@
+sudo docker run --name sqlserver2017anp -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=SqlServer2017!' -p 1433:1433 -v /home/renatogroffe/Desenvolvimento/Interop-Summit/Docker/Volumes/SQLServer2017:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2017-latest
